555076312895ae0e0706f7d163798d00626a988c
Kirill <kkovalskis@inbox.lv>
2019-12-22 22:51:33 +0200

    adding react app

---
+++ precache-manifest.d21fc858a67fdd7e8bca16fa9997ad08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f8b23160cf45677df3394a2688f3ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2bd92906305864e41aa",
+    "url": "./static/css/main.f60f8626.chunk.css"
+  },
+  {
+    "revision": "49c72a6b4142ebc5b204",
+    "url": "./static/js/2.f7fe0d17.chunk.js"
+  },
+  {
+    "revision": "d2bd92906305864e41aa",
+    "url": "./static/js/main.0e1a43ad.chunk.js"
+  },
+  {
+    "revision": "9b4722a664b10dbb0af3",
+    "url": "./static/js/runtime-main.fdf3a90d.js"
+  }
+]);
